a74deb1f2853d372a3978d1f8cfd7fb213e79cc7
davyson <davyssonlevi2.0@gmail.com>
2026-03-23 11:56:34 -0300

    ajeitando-cursos

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,21 @@
 //função de lavar para outra aba
 function irPara(caminhoDaPagina){
     window.location.href = caminhoDaPagina
-}   
+}
+
+//função para abrir formulario
+function formularioParaGuia(objProfessores){
+    const form = document.createElement("form").setAttribute("id", "formularioGuia")
+    //select para metodologia
+    const selectMetodologia = document.createElement("select").setAttribute("id", "metodologia")
+    //dentro do select
+    const optAtiva = document.createElement("option").setAttribute("value", 1)
+    const optConstrutiva = document.createAttribute("option").setAttribute("value", 2)
+    //select para professores
+    const selectProfessores = document.createElement("select").setAttribute("id", "professores")
+    //dentro do select
+    const optProfessores = objProfessores.forEach(o => {
+        let option = document.createElement("option").setAttribute(o)
+    })
+
+}
