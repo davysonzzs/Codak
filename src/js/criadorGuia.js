24375de3f9034abb871738dd5e43897484908aca
davyson <davyssonlevi2.0@gmail.com>
2026-03-25 10:26:27 -0300

    stages-concluidos

--- src/js/criadorGuia.js
+++ src/js/criadorGuia.js
@@ -159,3 +159,89 @@ const stageJs = {
         5 * 10
         `, "link para documentação: https://www.w3schools.com/js/js_syntax.asp", " exercicio disponivel: https://pt.anotepad.com/note/read/t237ic9d"]
 }
+
+const stageHtml = {
+    "stage 1: Introdução": [
+        `
+        O que é HTML?
+
+        HTML significa Hyper Text Markup Language (Linguagem de Marcação de Hipertexto).
+        HTML é a linguagem de marcação padrão para a criação de páginas web.
+        HTML descreve a estrutura de uma página da Web.
+        O HTML consiste em uma série de elementos.
+        Os elementos HTML informam ao navegador como exibir o conteúdo.
+        Os elementos HTML rotulam partes do conteúdo, como "isto é um título", "isto" é um parágrafo”, "isto é um link", etc.
+
+        Um documento HTML simples
+
+        Exemplo
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Page Title</title>
+        </head>
+        <body>
+
+        <h1>My First Heading</h1>
+        <p>My first paragraph.</p>
+
+        </body>
+        </html> 
+
+        O <!DOCTYPE html>declaração define que este documento é um documento HTML5
+        O <html>O elemento raiz é o elemento raiz de um HTML. página
+        O <head>O elemento contém metadados sobre o Página HTML
+        O <title>O elemento especifica um título para o Página HTML (que é exibida na barra de título do navegador ou na aba da página)
+        O <body>O elemento define o O corpo do documento é um contêiner para todo o conteúdo visível, como: Títulos, parágrafos, imagens, hiperlinks, tabelas, listas, etc.
+        O <h1>O elemento define um título grande.
+        O <p>O elemento define um parágrafo.
+
+        O que é um elemento HTML?
+
+        Um elemento HTML é definido por uma tag de abertura, algum conteúdo e uma tag de fechamento:
+        < tagname > O conteúdo vai aqui... </tagname>
+
+        O elemento HTML abrange tudo desde a tag de abertura até a tag de fechamento:
+        < h1 > Meu Primeiro título</h1> 
+        <p> Meu primeiro parágrafo </p> 
+        `, "Link para documentação: https://www.w3schools.com/html/html_intro.asp", "Exercicio disponivel: https://pt.anotepad.com/note/read/qwsai3wp"],
+        "stage 2: Basico": [
+            `
+            Títulos HTML
+
+            Os cabeçalhos HTML são definidos com o <h1> para <h6>etiquetas.
+            <h1>define o título mais importante. <h6>define o menos importante cabeçalho:
+
+            Exemplo
+            <h1>Eu sou um Titulo</h1>
+            <h2>Eu sou um subTitulo</h2>
+            <h3>Eu sou um texto-menorzinho</h3> 
+
+            Parágrafos HTML
+
+            Os parágrafos HTML são definidos com o <p> marcação:
+
+            Exemplo
+            <p>Eu sou um Paragrafo</p>
+            <p>Eu sou outro Paragrafo</p>
+            
+            Links HTML
+
+            Os links HTML são definidos com o <a> marcação:
+
+            Exemplo
+            <a href="#">isso é um link</a>
+            O destino do link é especificado no href.
+            Os atributos são usados para fornecer informações adicionais sobre elementos HTML.
+            Você aprenderá mais sobre atributos mais para frente
+
+            Imagens HTML
+
+            As imagens HTML são definidas com o <img> marcação.
+            O arquivo de origem ( src), texto alternativo ( alt), width, e heightsão fornecidos como atributos:
+
+            Exemplo
+            <img src="Codak.jpg" alt="Codak-logo" width="104" height="142"> 
+            `, "link para documentação: https://www.w3schools.com/html/html_basic.asp", "Exercicio disponivel: https://pt.anotepad.com/note/read/p2qqs34x"]
+}
+
